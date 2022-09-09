500faa1b33e5be5178e3026a7e2a57059f95afd1
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-09 09:44:03 +0200

    first commit

---
+++ HelloWorld.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpDiscovery.Quest01
+{
+  public class HelloWorld_Exercice
+  {
+    public static string HelloWorld()
+    {
+      return "Hello World !";
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CSharpDiscovery.Quest01;
+
+namespace TestCSharp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine(HelloWorld_Exercice.HelloWorld());
+    }
+  }
+}
