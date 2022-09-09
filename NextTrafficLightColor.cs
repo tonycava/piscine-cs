2d2120d93e314423fb550622a48e0cc985c3925c
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-09 12:41:21 +0200

    NextTrafficLightColor.cs enough ?

--- NextTrafficLightColor.cs
+++ NextTrafficLightColor.cs
@@ -6,8 +6,8 @@ namespace CSharpDiscovery.Quest01
   {
     public static TrafficLightColor GetNextTrafficLightColor(TrafficLightColor currentColor)
     {
-      if (currentColor == TrafficLightColor.Red) return TrafficLightColor.None;
-      
+      if (currentColor == TrafficLightColor.None) return TrafficLightColor.None;
+      if (currentColor == TrafficLightColor.Red) return TrafficLightColor.Green;
       return (TrafficLightColor) ((int) (currentColor) + 1);
     }
   }
