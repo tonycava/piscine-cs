8845af7d898396213b42562bb6a20330b448ca52
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-09 12:14:49 +0200

    commit

---
+++ Capitalize.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharpDiscovery.Quest01
+{
+  public class Capitalize_Exercice
+  {
+    public static string Capitalize(string str)
+    {
+      if (str == null) return 0;
+      return str.ToUpper();
+    }
+  }
+}
