02b9775fd5c9098fec8b05e6741b254b55f685e6
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-12 08:26:38 +0200

    DifferenceInMinutes.cs add

---
+++ DifferenceInMinutes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharpDiscovery.Quest02
+{
+  public class DifferenceInMinutes_Exercice
+  {
+    public static double DifferenceInMinutes(DateTime start, DateTime end)
+    {
+      var ts = end - start;
+      return ts.TotalMinutes;
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,10 @@ namespace TestCSharp
   {
     static void Main(string[] args)
     {
-      Console.WriteLine(TodayAtMidnight_Exercice.TodayAtMidnight());
+      var start = new DateTime(2021, 02, 01, 12, 0, 0, 0);
+      var end = new DateTime(2021, 02, 01, 18, 0, 0, 0);
+      Console.WriteLine(DifferenceInMinutes_Exercice.DifferenceInMinutes(start, end));
+
     }
   }
 }
