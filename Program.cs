7f8b394df29363515b3c7fb128c3256ac0711fbd
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-12 08:44:33 +0200

    SortTable.cs add

--- Program.cs
+++ Program.cs
@@ -7,10 +7,8 @@ namespace TestCSharp
   {
     static void Main(string[] args)
     {
-      var tab = new int[5] { 99, 98, 92, 97, 95 };
-      var tab2 = new int[5] { 50, 54, 85, 47, 2 };
-      Console.WriteLine(ContainsThisValue_Exercice.ContainsThisValue(tab, 97));
-      Console.WriteLine(ContainsThisValue_Exercice.ContainsThisValue(tab2, 51));
+      var array = new int[] { 3, 24, 13, 4, 40 };
+      Console.WriteLine(string.Join(", ", SortTable_Exercice.SortTable(array)));
     }
   }
 }
---
+++ SortTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpDiscovery.Quest02
+{
+  public class SortTable_Exercice
+  {
+    public static int[] SortTable(int[] tab)
+    {
+      Array.Sort(tab);
+      return tab;
+    }
+  }
+}
