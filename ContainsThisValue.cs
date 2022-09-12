2d1a452e9d4b40034e29e2cf2ada2916e5f760dc
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-12 08:38:09 +0200

    ContainsThisValue.cs add

---
+++ ContainsThisValue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace CSharpDiscovery.Quest02
+{
+  public class ContainsThisValue_Exercice
+  {
+    public static bool ContainsThisValue(int[] tab, int value)
+    {
+      return tab.Contains(value);
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,10 @@ namespace TestCSharp
   {
     static void Main(string[] args)
     {
-      Console.WriteLine(FromStringToDateTime_Exercice.FromStringToDateTime("21/12/2021 18:33"));
+      var tab = new int[5] { 99, 98, 92, 97, 95 };
+      var tab2 = new int[5] { 50, 54, 85, 47, 2 };
+      Console.WriteLine(ContainsThisValue_Exercice.ContainsThisValue(tab, 97));
+      Console.WriteLine(ContainsThisValue_Exercice.ContainsThisValue(tab2, 51));
     }
   }
 }
