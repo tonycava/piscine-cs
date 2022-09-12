c1fa8accb4a3594407b6f970306e79355e02d634
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-12 11:03:46 +0200

    new change 2

--- SecretSanta.cs
+++ SecretSanta.cs
@@ -1,48 +1,22 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
-
-namespace CSharpDiscovery.Quest02
-{
-  public class SecretSanta_Exercice
-  {
-    public static Dictionary<string,string> SecretSantaDraw(HashSet<string> people)
-    {
-      Console.WriteLine(string.Join("==>", people));
-      
-      Dictionary<string, string> dic = new Dictionary<String, String>();
-      
-      
-      if (people.Count == 2)
-      {
-        dic.Add(people.ElementAt(0), people.ElementAt(1));
-        return dic;
-      }
-      
-      Random rnd = new Random();
-      
-      for (int i = 0; i < (people.Count - 1) / 2; i+=2)
-      {
-        int indexOfPerson1 = rnd.Next(0, people.Count);
-        int indexOfPerson2 = rnd.Next(0, people.Count);
-        
-        while (indexOfPerson1 == indexOfPerson2) indexOfPerson2 = rnd.Next(0, people.Count);
-
-        if (dic.ContainsKey(people.ElementAt(indexOfPerson1)) == false)
-        {
-          
-          if (indexOfPerson2 > indexOfPerson1) (indexOfPerson1, indexOfPerson2) = (indexOfPerson2, indexOfPerson1);
-
-          dic.Add(people.ElementAt(indexOfPerson1), people.ElementAt(indexOfPerson2));
-            
-          if (people.Count == 2) break;
-            
-          people.Remove(people.ElementAt(indexOfPerson1));
-          people.Remove(people.ElementAt(indexOfPerson2));
+using System.Linq;
+namespace CSharpDiscovery.Quest02 {
+  public class SecretSanta_Exercice {
+    public static Dictionary<string, string> SecretSantaDraw(HashSet<string> people) {
+      Random rdm = new Random();
+      Dictionary<string, string> result = new();
+      int counter = 0;
+      var shuffled = people.ToArray().OrderBy(s => rdm.Next()).ToArray();
+      foreach (string s in people) {
+        while (s == shuffled[0]) {
+          shuffled = shuffled.ToArray().OrderBy(s => rdm.Next()).ToArray();
         }
+        result[s] = shuffled[0];
+        shuffled = shuffled.Where(it => it != shuffled[0]).ToArray();
+        counter++;
       }
-      dic.Add(people.ElementAt(0), people.ElementAt(1));
-      return dic;
+      return result;
     }
   }
 }
