e3769bb8cf72faa29a724cc08640883fcc5a1861
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-12 11:13:03 +0200

    SecretSanta.cs good enought ?

--- SecretSanta.cs
+++ SecretSanta.cs
@@ -1,22 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-namespace CSharpDiscovery.Quest02 {
-  public class SecretSanta_Exercice {
-    public static Dictionary<string, string> SecretSantaDraw(HashSet<string> people) {
-      Random rdm = new Random();
-      Dictionary<string, string> result = new();
-      int counter = 0;
-      var shuffled = people.ToArray().OrderBy(s => rdm.Next()).ToArray();
-      foreach (string s in people) {
-        while (s == shuffled[0]) {
-          shuffled = shuffled.ToArray().OrderBy(s => rdm.Next()).ToArray();
-        }
-        result[s] = shuffled[0];
-        shuffled = shuffled.Where(it => it != shuffled[0]).ToArray();
-        counter++;
+using System.Collections.Generic;
+
+namespace CSharpDiscovery.Quest02
+{
+  public class SecretSanta_Exercice
+  {
+    public static Dictionary<string,string> SecretSantaDraw(HashSet<string> people)
+    {
+      Console.WriteLine(string.Join("==>", people));
+      
+      Dictionary<string, string> dic = new Dictionary<String, String>();
+      
+      if (people.Count == 2)
+      {
+        dic.Add(people.ElementAt(0), people.ElementAt(1));
+        dic.Add(people.ElementAt(1), people.ElementAt(0));
+        return dic;
       }
-      return result;
+      
+      Random rnd = new Random();
+      
+      for (int i = 0; i < people.Count; i++)
+      {
+        string person = people.ElementAt(i);
+        int indexOfPerson = rnd.Next(0, people.Count);
+
+        if (dic.ContainsKey(person) == false)
+        {
+          dic.Add(person, people.ElementAt(indexOfPerson));
+        }
+      }
+      return dic;
     }
   }
 }
