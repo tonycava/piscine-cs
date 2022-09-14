89037d4ef1aa9842332830c598513f33297c138f
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-14 08:23:37 +0200

    new commit

---
+++ PointOfInterest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CSharpDiscovery.Quest03;
+
+namespace TestCSharp
+{
+  class Program
+  {
+    static void Main(string[] str)
+    {
+      var poi = new PointOfInterest();
+      Console.WriteLine(poi.Latitude);
+      Console.WriteLine(poi.Longitude);
+      Console.WriteLine(poi.Name);
+
+      Console.WriteLine(PointOfInterest.GoogleMapsUrlTemplate);
+    }
+  }
+  public class PointOfInterest
+  {
+    public double Latitude = 0;   
+    public double Longitude  = 0;   
+    public string Name = "";
+
+    public static string GoogleMapsUrlTemplate()
+    {
+      return "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
+    }
+    
+  }
+}
--- SecretSanta.cs
+++ SecretSanta.cs
@@ -8,27 +8,18 @@ namespace CSharpDiscovery.Quest02
   {
     public static Dictionary<string,string> SecretSantaDraw(HashSet<string> people)
     {
-      Console.WriteLine(string.Join("==>", people));
-      
       Dictionary<string, string> dic = new Dictionary<String, String>();
-      
-      if (people.Count == 2)
-      {
-        dic.Add(people.ElementAt(0), people.ElementAt(1));
-        dic.Add(people.ElementAt(1), people.ElementAt(0));
-        return dic;
-      }
-      
       Random rnd = new Random();
       
       for (int i = 0; i < people.Count; i++)
       {
         string person = people.ElementAt(i);
-        int indexOfPerson = rnd.Next(0, people.Count);
+        string person2 = people.ElementAt(rnd.Next(0, people.Count));
 
-        if (dic.ContainsKey(person) == false)
-        {
-          dic.Add(person, people.ElementAt(indexOfPerson));
+        while (person == person2) person2 = people.ElementAt(rnd.Next(0, people.Count));
+        
+        if (!dic.ContainsKey(person)){
+          dic.Add(person, person2);
         }
       }
       return dic;
