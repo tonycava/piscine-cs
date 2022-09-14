b56fe0b59d22dfcb635bf8184599b2855b5685ef
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-14 08:32:28 +0200

    PointOfInterest.cs add

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -20,10 +20,7 @@ namespace CSharpDiscovery.Quest03
     public double Longitude  = 0;   
     public string Name = "";
 
-    public static string GoogleMapsUrlTemplate()
-    {
-      return "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
-    }
-    
+    public static string GoogleMapsUrlTemplate = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
+
   }
 }
