d64f9ad5a500dcb62648f340a7f63ab98039ebf1
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-14 08:44:27 +0200

    good enought ?

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -16,11 +16,11 @@ namespace CSharpDiscovery.Quest03
   }
   public class PointOfInterest
   {
-    public double Latitude {get} = 0;   
-    public double Longitude {get} = 0;   
-    public string Name {get} = "";
+    public double Latitude { get; } = 0;   
+    public double Longitude { get; } = 0;   
+    public string Name { get; } = "";
 
-    public static string GoogleMapsUrlTemplate {get} = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
+    public static string GoogleMapsUrlTemplate { get; } = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
 
   }
 }
