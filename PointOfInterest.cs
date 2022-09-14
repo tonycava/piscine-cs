83030cc4ef20d39ce5d0dc479344d44fc09159dd
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-14 08:43:26 +0200

    enought2?

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -2,13 +2,25 @@
 
 namespace CSharpDiscovery.Quest03
 {
+  class Program
+  {
+    static void Main(string[] str)
+    {
+      var poi = new PointOfInterest();
+      Console.WriteLine(poi.Latitude);
+      Console.WriteLine(poi.Longitude);
+      Console.WriteLine(poi.Name);
+
+      Console.WriteLine(PointOfInterest.GoogleMapsUrlTemplate);
+    }
+  }
   public class PointOfInterest
   {
-    public double Latitude = 0;   
-    public double Longitude  = 0;   
-    public string Name = "";
+    public double Latitude {get} = 0;   
+    public double Longitude {get} = 0;   
+    public string Name {get} = "";
 
-    public static string GoogleMapsUrlTemplate = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
+    public static string GoogleMapsUrlTemplate {get} = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
 
   }
 }
