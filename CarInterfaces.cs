10cc98cad274637149ce3ba97e993afe2534b707
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-16 09:19:35 +0200

    ok ?

---
+++ CarInterfaces.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharpDiscovery.Quest04
+{
+  public interface IThermalCar
+  {
+    public int FuelLevel;
+    
+    public void FillUp():
+    public int GetFuelLevel():
+    
+  }
+  public interface IElectricCar
+  {
+    public int BatteryLevel;
+    public void Recharge();
+    public int GetBatteryLevel();
+    
+  }
+}
