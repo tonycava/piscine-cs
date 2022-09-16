986fea95c9989399d15441f92f1987e4612a30a3
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-16 10:31:00 +0200

    ok ?

---
+++ HybridCar.cs
@@ -0,0 +1,66 @@
+﻿using CSharpDiscovery.Quest04;
+using System;
+
+namespace TestCSharp
+{
+  class HybridCar : Car, IThermalCar
+  {
+    public HybridCar() {}
+
+    public HybridCar(string model, string brand, string color, int currentSpeed = 0) : base(model, brand, color,
+      currentSpeed) {}
+
+
+    private int _FuelLevel = 100;
+    private int _BatteryLevel = 100;
+    
+    public int FuelLevel
+    {
+      get { return _FuelLevel; }
+      set { _FuelLevel = value;  }
+    }
+    public void FillUp()
+    {
+      FuelLevel = 100;
+    }
+    public int GetFuelLevel()
+    {
+      return FuelLevel;
+    }
+
+    public int BatteryLevel
+    {
+      get { return _BatteryLevel; }
+      set { _BatteryLevel = value;  }
+    }
+    public void Recharge()
+    {
+      BatteryLevel = 100;
+    }
+    public int GetBatteryLevel()
+    {
+      return FuelLevel;
+    }
+    public override string ToString()
+    {
+
+
+        
+      return $"{Color} {Brand} {Model}, Battery: {BatteryLevel}%, Fuel: {FuelLevel}%";
+    }
+    
+    
+    static void Main(string[] str)
+    {
+      var i8 = new HybridCar("i8", "BMW", "White");
+      i8.BatteryLevel = 60;
+      i8.FuelLevel = 80;
+      Vehicule.WhoIsHere();
+
+      i8.Recharge();
+      i8.FillUp();
+      Console.WriteLine(i8.GetBatteryLevel());
+      Console.WriteLine(i8.GetFuelLevel());
+    }
+  }
+}
--- Truck.cs
+++ Truck.cs
@@ -42,21 +42,4 @@ namespace CSharpDiscovery.Quest04
   }
   
   
-  class Program
-  {
-    static void Main(string[] str)
-    {
-      var RandomTruck = new Truck();
-      var MAC = new Truck(2, "MAC", "White", 80);
-      var Renault = new Truck(3, "Renault", "Black", 20);
-
-      Vehicule.WhoIsHere();
-
-      RandomTruck.Accelerate(30);
-      MAC.Accelerate(40);
-      Renault.Brake(30);
-
-      Vehicule.WhoIsHere();
-    }
-  }
 }
