7b25c691ff27ffe17c8fc3488b964ede9751b43f
ACAVAGNE <91544506+WeZeOx@users.noreply.github.com>
2022-09-16 09:12:51 +0200

    ok ?

--- Car.cs
+++ Car.cs
@@ -40,26 +40,6 @@ namespace CSharpDiscovery.Quest04
     }
     
     
-    static void Main(string[] str)
-    {
-      var Megane = new Car("Mégane", "Renault", "Yellow", 100);
-
-      Megane.Accelerate(50);
-
-      Console.WriteLine(Megane.CurrentSpeed);
-
-      Megane.Accelerate(50);
-
-      Console.WriteLine(Megane.CurrentSpeed);
-
-      Megane.Brake(160);
-
-      Console.WriteLine(Megane.CurrentSpeed);
-
-      Megane.Brake(40);
-
-      Console.WriteLine(Megane.CurrentSpeed);
-    }
   }
   
 }
---
+++ Truck.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace CSharpDiscovery.Quest04
+{
+
+  public class Truck : Vehicule
+  {
+
+    public int Tonnage { get; set; } = 0;
+
+    public Truck()
+    {
+      Tonnage = 0;
+    }    
+    
+    public Truck(int tonnage, string brand, string color, int currentSpeed = 0)
+    {
+      Tonnage = tonnage;
+      Brand = brand;
+      Color = color;
+      CurrentSpeed = currentSpeed;
+    }
+
+
+    public override string ToString()
+    {
+      return $"{Color} {Brand} {Tonnage}T Truck";
+    }
+    
+    
+    public override void Accelerate(int newSpeed)
+    {
+      if (CurrentSpeed + newSpeed > 100) CurrentSpeed = 180;
+      else CurrentSpeed += newSpeed;
+    }    
+    
+    public override void Brake(int newSpeed)
+    {
+      if (CurrentSpeed - newSpeed < 0) CurrentSpeed = 0;
+      else CurrentSpeed -= newSpeed;
+    }
+  }
+  
+  
+  class Program
+  {
+    static void Main(string[] str)
+    {
+      var RandomTruck = new Truck();
+      var MAC = new Truck(2, "MAC", "White", 80);
+      var Renault = new Truck(3, "Renault", "Black", 20);
+
+      Vehicule.WhoIsHere();
+
+      RandomTruck.Accelerate(30);
+      MAC.Accelerate(40);
+      Renault.Brake(30);
+
+      Vehicule.WhoIsHere();
+    }
+  }
+}
